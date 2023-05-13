04438052e80ffb701ddbae1a1905dd9d01da06b1
Eriquy Jhordan <eriquy@gmail.com>
2023-05-13 18:17:23 -0300

    remove gallery from mobile

--- src/components/Family/index.tsx
+++ src/components/Family/index.tsx
@@ -22,9 +22,9 @@ export default function Family() {
           <div className={styles.familyTexts}>
             <p className={`${poppins.className} ${styles.familyLeadText}`}>Família é a base de tudo</p>
             <h2 className={`${inter.className} ${styles.familyTitle}`}>Eu e a minha casa serviremos ao SENHOR!</h2>
-            <div className={styles.mobileGallery}>
+            {/* <div className={styles.mobileGallery}>
               <Gallery />
-            </div>
+            </div> */}
             <p className={`${poppins.className} ${styles.familyParagraph}`}>Na Igreja Renovada, consideramos a família como a base fundamental de todos os valores morais e espirituais que transmitimos a futuras gerações. É por isso que o versículo de Josué 24:15 é tão importante para nós: &ldquo;Eu e minha casa serviremos ao Senhor&ldquo;.</p>
             <p className={`${poppins.className} ${styles.familyParagraph}`}>Como ensinado por Jesus em Lucas 8:21, &ldquo;… minha mãe e meus irmãos são aqueles que ouvem a palavra de Deus e a põem em prática.&ldquo; Viver conforme a palavra de Deus em casa e fora dela, colocando em prática seus princípios, é fundamental para que a promessa em Atos 16:31 se torne realidade em nossas vidas: &ldquo;... Crê no Senhor Jesus Cristo e serás salvo, tu e a tua casa.&ldquo;</p>
             <a href="#" className={`${poppins.className} ${styles.familyLink}`}>

c958685380ab06febfb8c9d9ee190656401d06f2
Eriquy Jhordan <eriquy@gmail.com>
2023-05-13 09:25:59 -0300

    feat: add reset css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
-import { Inter } from 'next/font/google'
+import { Inter, Poppins } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({ subsets: ['latin'], weight: ['400', '700'], })
 
 export default function Home() {
   return (
     <>
-      <h1>First commit</h1>
+      <h1 className={poppins.className}>First commit</h1>
     </>
   )
 }

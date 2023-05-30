c233b045d2159f8d5dda3999f1cda9c8cd43c4c4
Eriquy Jhordan <eriquy@gmail.com>
2023-05-30 06:06:16 -0300

    fix: background color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,15 @@
 import Bible from '@/components/Blible'
 import Family from '@/components/Family'
 import Header from '@/components/header'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <>
-      <Header />
-      <Family />
+      <div className={styles.familyWrapper}>
+        <Header />
+        <Family />
+      </div>
       <Bible />
     </>
   )

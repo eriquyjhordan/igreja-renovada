c9543cbe34914053447d0d6dddd300a855c05751
Eriquy Jhordan <eriquy@gmail.com>
2023-05-30 10:36:36 -0300

    feat: start the blog section

--- src/components/Blible/index.tsx
+++ src/components/Blible/index.tsx
@@ -1,7 +1,30 @@
+import Image from 'next/image'
+import { Inter, Poppins } from 'next/font/google'
 import styles from './styles.module.css'
+
+
+const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({ subsets: ['latin'], weight: ['400', '500', '600', '700'], })
 
 export default function Bible() {
   return (
-    <div className={styles.container}></div>
+    <div className={styles.container}>
+      <div className={styles.blogHeader}>
+        <div>
+          <p className={`${poppins.className} ${styles.blogLead}`}>Blog</p>
+          <h3 className={`${inter.className} ${styles.blogTitle}`}>Estudos Biblicos</h3>
+        </div>
+        <a href='#' className={`${poppins.className} ${styles.blobLink}`}>
+          <Image 
+            src='/blog-icon.svg'
+            alt='blog icon'
+            width={20}
+            height={20}
+            style={{marginLeft: '0.625rem'}}
+          />
+          Veja todos os estudos
+        </a>
+      </div>
+    </div>
   )
 }
